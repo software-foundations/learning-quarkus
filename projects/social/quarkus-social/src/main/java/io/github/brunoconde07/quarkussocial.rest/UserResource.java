bbc7e46a0e20e513e738807e78adf15877cdfc1e
brunoconde07 <brunoconde.ufpa@gmail.com>
2022-07-14 15:34:14 -0300

    [project-social] -> class 13-15

---
+++ projects/social/quarkus-social/src/main/java/io/github/brunoconde07/quarkussocial.rest/UserResource.java
@@ -0,0 +1,23 @@
+package io.github.brunoconde07.quarkussocial.rest;
+
+import io.github.brunoconde07.quarkussocial.rest.dto.CreateUserRequest;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/users")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public class UserResource {
+
+	@POST
+	public Response createUser( CreateUserRequest userRequest ) {
+		return Response.ok(userRequest).build();
+	}
+
+	@GET
+	public Response listAllUsers() {
+		return Response.ok().build();
+	}
+}
