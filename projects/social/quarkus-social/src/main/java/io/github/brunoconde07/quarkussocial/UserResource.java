de297d8286aa409cd88c7859a76f7430cf4814b5
brunoconde07 <brunoconde.ufpa@gmail.com>
2022-07-23 16:32:43 -0300

    [project-social] -> class 22

--- projects/social/quarkus-social/src/main/java/io/github/brunoconde07/quarkussocial/UserResource.java
+++ projects/social/quarkus-social/src/main/java/io/github/brunoconde07/quarkussocial/UserResource.java
@@ -10,9 +10,12 @@ import org.h2.command.ddl.CreateUser;
 
 import javax.inject.Inject;
 import javax.transaction.Transactional;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validator;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.Set;
 
 @Path("/users")
 @Consumes(MediaType.APPLICATION_JSON)
@@ -20,16 +23,29 @@ import javax.ws.rs.core.Response;
 public class UserResource {
 
 	private UserRepository repository;
+	private Validator validator;
 
 	@Inject
-	public UserResource(UserRepository repository){
+	public UserResource(UserRepository repository, Validator validator){
 		this.repository = repository;
+		this.validator = validator;
 	}
 
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Transactional
 	public Response createUser( CreateUserRequest userRequest ) {
+
+		Set<ConstraintViolation<CreateUserRequest>> violations = validator.validate(userRequest);
+
+		if ( !violations.isEmpty() ) {
+
+			ConstraintViolation<CreateUserRequest> error = violations.stream().findAny().get();
+
+			String errorMessage = error.getMessage();
+
+			return Response.status(400).entity(errorMessage).build();
+		}
 
 		User user = new User();
 
--- projects/social/quarkus-social/src/main/java/io/github/brunoconde07/quarkussocial/dto/CreateUserRequest.java
+++ projects/social/quarkus-social/src/main/java/io/github/brunoconde07/quarkussocial/dto/CreateUserRequest.java
@@ -1,7 +1,14 @@
 package io.github.brunoconde07.quarkussocial.dto;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 public class CreateUserRequest {
+
+	@NotBlank( message = "Name is Required" )
 	private String name;
+
+	@NotNull( message = "Age is Required" )
 	private Integer age;
 
 	public String getName() {
