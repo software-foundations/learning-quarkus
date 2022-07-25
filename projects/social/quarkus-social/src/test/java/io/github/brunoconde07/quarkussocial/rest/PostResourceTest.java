a3f8ec8086daa4d3fee9e8c90af912fd9bb16753
brunosilva5 <bruno.silva@sysdata.com.br>
2022-07-25 16:32:12 -0300

    [project-social] -> class 42

---
+++ projects/social/quarkus-social/src/test/java/io/github/brunoconde07/quarkussocial/rest/PostResourceTest.java
@@ -0,0 +1,60 @@
+package io.github.brunoconde07.quarkussocial.rest;
+
+import io.github.brunoconde07.quarkussocial.domain.model.User;
+import io.github.brunoconde07.quarkussocial.domain.repository.UserRepository;
+import io.github.brunoconde07.quarkussocial.rest.dto.CreatePostRequest;
+import io.quarkus.test.common.http.TestHTTPEndpoint;
+import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import javax.inject.Inject;
+import javax.transaction.Transactional;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.*;
+
+@QuarkusTest
+@TestHTTPEndpoint(PostResource.class) // allows us do not pass the url in the http request test
+class PostResourceTest {
+
+    @Inject
+    UserRepository userRepository;
+    Long userId;
+
+    @BeforeEach
+    @Transactional
+    public void setUP() {
+
+        User user = new User();
+
+        user.setName("John");
+
+        user.setAge(20);
+
+        userRepository.persist(user);
+
+        userId = user.getId();
+
+    }
+
+    @Test
+    @DisplayName("should create a post for a user")
+    public void createPostTest() {
+
+        CreatePostRequest postRequest = new CreatePostRequest();
+
+        postRequest.setText("Some text");
+
+        given()
+                .contentType(ContentType.JSON)
+                .body(postRequest)
+                .pathParam("userId", userId)
+                .when()
+                .post()
+                .then()
+                .statusCode(201);
+    }
+}
